2eee6423f6d0cd3f91666025608415447eb9615f
hot8118@naver.com <hot8118@naver.com>
2021-02-06 01:43:37 +0900

    JPA, PostsRepository added PostsRepository Test completed (java src)..

---
+++ src/test/java/com/limky/admin/springboot/test/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.limky.admin.springboot.test.domain.posts;
+
+import com.limky.admin.springboot.domain.posts.Posts;
+import com.limky.admin.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void posts_load(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        //테이블 posts에 insert/update쿼리를 수행
+        //id 값이 있다면 update 없다면 insert
+        postsRepository.save(Posts.builder()
+                                    .title(title)
+                                    .content(content)
+                                    .author("setupman82@gmail.com")
+                                    .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+
+    }
+
+
+}
