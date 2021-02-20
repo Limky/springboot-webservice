e402644c04d314ae05a7f15ffa4a272fd8e4d727
hot8118@naver.com <hot8118@naver.com>
2021-02-21 03:05:00 +0900

    게시글 수정 구현 but 버그 해결 전

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -2,13 +2,7 @@ var main = {
     init : function () {
         var _this = this;
         $('#btn-save').on('click', function () {
-            console.log('함수진입전');
-
             _this.save();
-
-            console.log('함수진입후');
-
-
         });
 
         $('#btn-update').on('click', function () {
@@ -20,9 +14,6 @@ var main = {
         });
     },
     save : function () {
-
-        console.log('함수진입');
-
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -42,43 +33,28 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
-    update : function () {
-        var data = {
-            title: $('#title').val(),
-            content: $('#content').val()
-        };
 
-        var id = $('#id').val();
+        update : function () {
+            var data = {
+                title: $('#title').val(),
+                content: $('#content').val()
+            };
 
-        $.ajax({
-            type: 'PUT',
-            url: '/api/v1/posts/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 수정되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    },
-    delete : function () {
-        var id = $('#id').val();
+            var id = $('#id').val();
 
-        $.ajax({
-            type: 'DELETE',
-            url: '/api/v1/posts/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8'
-        }).done(function() {
-            alert('글이 삭제되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
-    }
-
+            $.ajax({
+                type: 'PUT',
+                url: '/api/v1/posts/'+id,
+                dataType: 'json',
+                contentType:'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function() {
+                alert('글이 수정되었습니다.');
+                window.location.href = '/';
+            }).fail(function (error) {
+                alert(JSON.stringify(error));
+            });
+        }
 };
 
 main.init();
